27c97b167b5aa9d614b77c354baa63fe333ced85
bashthat <3249@holbertonstudents.com>
2022-11-01 18:29:07 -0500

    thescene

--- 0x00-unity-assets_models_textures/Assets/Scripts/Timer.cs
+++ 0x00-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -13,34 +14,54 @@ public class Timer : MonoBehaviour
     private int minutes = 0;
     private int hours = 0;
     private int days = 0;
-
-    public TextMeshProUGUI TimerText;
+    public Text TimerText;
+    private float timer = 0.0f;
+    private bool timerRunning = true;
+    public Text startTime;
+    private Stopwatch stopWatch = new Stopwatch();
 
     void Start()
     {
-        StartCoroutine(Playtime());
+        stopWatch.Start();
+        // StartCoroutine(Playtime());
     }
 
-    private IEnumerator Playtime()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(1);
-            playtime++;
-            seconds = playtime % 60;
-            minutes = (playtime / 60) % 60;
-            hours = (playtime / 3600) % 24;
-            days = (playtime / 86400) % 365;
-            TimerText.text = days.ToString("00") + ":" + hours.ToString("00") + ":" + minutes.ToString("00") + ":" + seconds.ToString("00");
-        }
-        void OnGUI()
-        {
-            if(GUI.Button(new Rect(100, 100, 40, 40), "Reset"))
-            {
-                playtime = 0;
-            }
+    //private IEnumerator Playtime()
+    //{
+        //while (true)
+        //{
+            //yield return new WaitForSeconds(1);
+            //playtime++;
+            //seconds = playtime % 60;
+            //minutes = (playtime / 60) % 60;
+            //hours = (playtime / 3600) % 24;
+            //days = (playtime / 86400) % 365;
+            //TimerText.text = days.ToString("00") + ":" + hours.ToString("00") + ":" + minutes.ToString("00") + ":" + seconds.ToString("00");
+        //}
+        //void OnGUI()
+        //{
+            //if(GUI.Button(new Rect(100, 100, 40, 40), "Reset"))
+            //{
+                //playtime = 0;
+            //}
 
-            GUI.Label(new Rect(50, 50, 400, 50), "Time: " + TimerText);
-        }
+            //GUI.Label(new Rect(50, 50, 400, 50), "Time: " + TimerText);
+        //}
+    
+
+    void Update()
+    {
+        // timer += Time.deltaTime;
+        TimerText.text = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+
+        // TimerText.text = stopWatch.Elapsed.ToString("hh\\:mm\\:ss");
+
+        
+    }
+    public void ResetTimer()
+    {
+        stopWatch.Reset();
+        stopWatch.Start();
+        
     }
 }
