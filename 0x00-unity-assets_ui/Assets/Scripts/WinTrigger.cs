5d6b8487388fe078e36ce86048f462f8debeae34
bashthat <3249@holbertonstudents.com>
2022-11-09 11:41:58 -0600

    assets

---
+++ 0x00-unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinTrigger : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+ 
+    
+    public GameObject winText;
+    public GameObject TimerText;
+    public GameObject player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            winText.SetActive(true);
+            TimerText.SetActive(false);
+            player.SetActive(false);
+        }
+    }
+    
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                
+    }
+}
+
